37ae9f22c08a5e92e99041d705eee378769a3686
Jamal <felipe.oliveira@avenue.us>
2024-09-18 14:50:53 -0300

    create a component of a storybook for only encapsulation of a story book

--- lib/src/widgets/storybook/storybook_mobile.dart
+++ lib/src/widgets/storybook/storybook_mobile.dart
@@ -1,4 +1,3 @@
-import 'package:device_frame/device_frame.dart';
 import 'package:flutter/material.dart';
 
 import '../../models/dto/attribute_dto.dart';
--- lib/storybook_ds.dart
+++ lib/storybook_ds.dart
@@ -3,3 +3,4 @@ library storybook_ds;
 export 'src/models/dto/attribute_dto.dart';
 export 'src/widgets/storybook/storybook_mobile.dart'
     if (dart.library.html) 'src/widgets/storybook/storybook_web.dart';
+export 'src/widgets/storybook/storybook_component.dart';
